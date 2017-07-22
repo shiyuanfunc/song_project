f9d4270525581a535e4f1d24ac772c1ed07e815a
song <songyh_1314@163.com>
2017-07-23 00:08:52 +0800

    加入shangchuan shuru fa

---
+++ src/Action/UploadBaseAction.java
@@ -0,0 +1,68 @@
+package Action;
+
+import org.apache.struts2.ServletActionContext;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+/**
+ * Created by song on 2017/7/22.
+ */
+public class UploadBaseAction {
+
+    private File upload;
+
+    private String uploadContentType;
+
+    private String uploadFileName;
+
+    private String savePath;
+
+    public String getSavePath() {
+        return ServletActionContext.getServletContext().getRealPath(savePath);
+    }
+
+    public void setSavePath(String savePath) {
+        this.savePath = savePath;
+    }
+
+    public File getUpload() {
+        return upload;
+    }
+
+    public void setUpload(File upload) {
+        this.upload = upload;
+    }
+
+    public String getUploadContentType() {
+        return uploadContentType;
+    }
+
+    public void setUploadContentType(String uploadContentType) {
+        this.uploadContentType = uploadContentType;
+    }
+
+    public String getUploadFileName() {
+        return uploadFileName;
+    }
+
+    public void setUploadFileName(String uploadFileName) {
+        this.uploadFileName = uploadFileName;
+    }
+
+    public String upload() throws Exception {
+
+        if (savePath != null) {
+            FileOutputStream fos = new FileOutputStream(getSavePath() + "\\" + getUploadFileName());
+            FileInputStream fis = new FileInputStream(getUpload());
+            byte[] buffer = new byte[1024];
+            int len = 0;
+            while ((len = fis.read(buffer)) > 0) {
+                fos.write(buffer, 0, len);
+            }
+            return "success";
+        } else {
+            return "error_path";
+        }
+    }
+}
