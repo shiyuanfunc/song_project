ccf7d7dc53e9770b9eebac1b42b8ddd1a89b19fa
song <songyh_1314@163.com>
2017-07-23 12:32:32 +0800

    jerry_smile@126.com
    Signed-off-by: song <songyh_1314@163.com>

---
+++ src/Action/TestActionInDownload.java
@@ -0,0 +1,23 @@
+package Action;
+public class TestActionInDownload {
+
+    private String name;
+
+    private String value;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "TestActionInDownload{" +
+                "name='" + name + '\'' +
+                ", value='" + value + '\'' +
+                '}';
+    }
+}
